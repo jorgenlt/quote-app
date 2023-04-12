00f457aafb30bd0bde74856a75ddf023a4b6c925
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-12 17:35:16 +0200

    deployed

--- src/quotesData.js
+++ src/quotesData.js
@@ -1,113 +1,103 @@
 const quotesData = [
-    { 
-        id: 1, 
-        quote: "Life is like a box of chocolates; you never know what you're gonna get.", 
-        author: "Forrest Gump" 
+    {
+        id: 1,
+        quote: "I am not a perfectionist, but I like to feel that things are done well. More important than that, I feel an endless need to learn, to improve, to evolve.",
+        author: "Cristiano Ronaldo"
     },
-    { 
-        id: 2, 
-        quote: "The only way to do great work is to love what you do.", 
-        author: "Steve Jobs" 
+    {
+        id: 2,
+        quote: "I've got a lot of mileage left in my legs and more to offer on and off the pitch.",
+        author: "David Beckham"
     },
-    { 
-        id: 3, 
-        quote: "The only thing we have to fear is fear itself.", 
-        author: "Franklin D. Roosevelt" 
+    {
+        id: 3,
+        quote: "I have to believe in myself. I know what I'm capable of.",
+        author: "Zlatan Ibrahimovic"
     },
-    { 
-        id: 4, 
-        quote: "In the middle of every difficulty lies opportunity.", 
-        author: "Albert Einstein" 
+    {
+        id: 4,
+        quote: "I am constantly being asked about individuals. The only way to win is as a team.",
+        author: "Pele"
     },
-    { 
-        id: 5, 
-        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.", 
-        author: "Winston Churchill" 
+    {
+        id: 5,
+        quote: "I learned all about life with a ball at my feet.",
+        author: "Ronaldinho"
     },
-    { 
-        id: 6, 
-        quote: "The best revenge is massive success.", 
-        author: "Frank Sinatra" 
+    {
+        id: 6,
+        quote: "I'm always trying to improve, to become more complete as a player.",
+        author: "Lionel Messi"
     },
-    { 
-        id: 7, 
-        quote: "The secret of getting ahead is getting started.", 
-        author: "Mark Twain" 
+    {
+        id: 7,
+        quote: "Football is a simple game; complicating it is what makes it bad.",
+        author: "Johan Cruyff"
     },
-    { 
-        id: 8, 
-        quote: "The only thing standing between you and your goal is the story you keep telling yourself.", 
-        author: "Jordan Belfort" 
+    {
+        id: 8,
+        quote: "In football, the result is an impostor. You can do things really, really well but not win. There's something greater than the result, more lasting: A legacy.",
+        author: "Xavi Hernandez"
     },
-    { 
-        id: 9, 
-        quote: "Life is change, growth is optional. Choose wisely.", 
-        author: "Karen Kaiser Clark" 
+    {
+        id: 9,
+        quote: "The harder you work, the harder it is to surrender.",
+        author: "Vince Lombardi"
     },
-    { 
+    {
         id: 10,
-        quote: "The best way to predict the future is to create it.", 
-        author: "Peter Drucker" 
+        quote: "Every morning you have two choices: continue to sleep with your dreams or wake up and chase them.",
+        author: "Eric Cantona"
     },
-    { 
+    {
         id: 11,
-        quote: "The only limit to our realization of tomorrow will be our doubts of today.", 
-        author: "Franklin D. Roosevelt" 
+        quote: "Football is like life - it requires perseverance, self-denial, hard work, sacrifice, dedication and respect for authority.",
+        author: "Vince Lombardi"
     },
-    { 
+    {
         id: 12,
-        quote: "It does not matter how slowly you go as long as you do not stop.", 
-        author: "Confucius" 
+        quote: "When you win, you don't get carried away. But if you go step by step, with confidence, you can go far.",
+        author: "Diego Maradona"
     },
-    { 
+    {
         id: 13,
-        quote: "Believe you can and you're halfway there.", 
-        author: "Theodore Roosevelt" 
+        quote: "I always say that goals are only important if they win games.",
+        author: "Ronaldo Nazario"
     },
-    { 
+    {
         id: 14,
-        quote: "The harder you work for something, the greater you'll feel when you achieve it.", 
-        author: "Unknown" 
+        quote: "I have failed again and again throughout my life. That's why I've been successful.",
+        author: "Michael Jordan"
     },
-    { 
+    {
         id: 15,
-        quote: "You are never too old to set another goal or to dream a new dream.", 
-        author: "C.S. Lewis" 
+        quote: "I don't believe in luck. I believe in hard work, dedication, and resilience.",
+        author: "Carli Lloyd"
     },
-    { 
+    {
         id: 16,
-        quote: "Don't watch the clock; do what it does. Keep going.", 
-        author: "Sam Levenson" 
+        quote: "Talent is important, but hard work can take you anywhere you want to go.",
+        author: "Alex Morgan"
     },
-    { 
+    {
         id: 17,
-        quote: "The best preparation for tomorrow is doing your best today.", 
-        author: "H. Jackson Brown Jr." 
+        quote: "The best preparation for winning in soccer is giving your best effort today.",
+        author: "Hope Solo"
     },
-    { 
+    {
         id: 18,
-        quote: "The only thing that stands between you and your dream is the will to try and the belief that it is actually possible.", 
-        author: "Joel Brown" 
+        quote: "The only thing that stands between you and your soccer dream is the determination to try and the perseverance to never give up, no matter the challenges you face.",
+        author: "Carli Lloyd"
     },
-    { 
+    {
         id: 19,
-        quote: "The future belongs to those who believe in the beauty of their dreams.", 
-        author: "Eleanor Roosevelt" 
+        quote: "Soccer is not just about kicking the ball, it's about having the courage to chase your dreams and never giving up.",
+        author: "Alex Morgan"
     },
-    { 
+    {
         id: 20,
-        quote: "Success is walking from failure to failure with no loss of enthusiasm.", 
-        author: "Winston Churchill" 
-    },
-    { 
-        id: 21,
-        quote: "The road to success and the road to failure are almost exactly the same.", 
-        author: "Colin R. Davis" 
-    },
-    { 
-        id: 22,
-        quote: "The best time to plant a tree was 20 years ago. The second best time is now.", 
-        author: "Chinese Proverb" 
+        quote: "The biggest risk in soccer is not taking any risks at all. Embrace challenges and push yourself beyond your comfort zone.",
+        author: "Jurgen Klopp"
     }
 ];
 
