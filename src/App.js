5141f394584ede7d44ed52be97bb717c763454cb
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-12 17:26:51 +0200

    deployed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <>
       <Quote />
       <a id="github" href="https://github.com/jorgenlt" target='_blank' rel="noreferrer">
-        <i class="fa-brands fa-github"></i>
+        <i className="fa-brands fa-github"></i>
       </a>
     </>
   );
