61c53fa2a92a3edaa4160c0fc1c3e129b85c7845
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-04-12 13:18:13 +0200

    finishing up

--- src/Quote.js
+++ src/Quote.js
@@ -1,69 +1,55 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import quotes from './quotesData'
 
-export default function Quote(props) {
+export default function Quote() {
     const randomNumber = (maxValue) => {
         return Math.floor(Math.random() * maxValue)
     };
     
     const randomQuote = quotes[randomNumber(quotes.length)];
-    
+
     const [currentQuote, setCurrentQuote] = React.useState(randomQuote);
-    
-    // const [focusIn, setFocusIn] = React.useState(true);
-    
-    // const stylesFocusIn = {
-    //     animation: "focus-in 800ms cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
-    // }
-    
-    // const stylesFocusOut = {
-    //     animation: "blur-out 800s cubic-bezier(0.550, 0.085, 0.680, 0.530) both"
-    // }
-    
-    // const focusIn = () => {
-    
-    // }
-    
-    // const nextQuote = () => {
-    //     setFocusIn((prevState) => {
-    //         return !prevState;
-    //     })
-    
-    //     const timer = () => {
-    //         setTimeout(
-    //             setCurrentQuote(() => quotes[randomNumber(quotes.length)]), 
-    //             5000
-    //         )
-    //     }
-    
-    // }
-    
-    // style={focusIn ? stylesFocusIn : stylesFocusOut}
-    
-    
-    
     const [isVisible, setIsVisible] = React.useState(true);
-    
+    const [copy, setCopy] = React.useState('unset');
     
     const toggleVisibility = () => {
         setIsVisible(!isVisible);
+    };
+
+    const toggleCopy = () => {
+        setCopy(!copy);
     };
     
     const nextQuote = () => {
         toggleVisibility();
         setCurrentQuote(() => quotes[randomNumber(quotes.length)]);
     }
-    
+
+    const copyQuote = () => {
+        console.log('copyQuote');
+        toggleCopy();
+        navigator.clipboard.writeText(`"${currentQuote.quote}" - ${currentQuote.author}`);
+
+    }
+
     return (
         <div id="quote-wrapper">
             <div className={isVisible ? 'focus-in' : 'focus-in2'}>
-                <div id="quote">
-                    <p>{randomQuote.quote}</p>
-                    <i className="fa-solid fa-quote-left" id="quote-left"></i>
-                    <i className="fa-solid fa-quote-right" id="quote-right"></i>
-                </div>
-                <div id="author">
-                    <p>- {currentQuote.author}</p>
+                <div>
+                    <div id="quote" onClick={() => copyQuote()}>
+                        <p id="random-quote">{currentQuote.quote}</p>
+                        <i className="fa-solid fa-quote-left" id="quote-left"></i>
+                        <i className="fa-solid fa-quote-right" id="quote-right"></i>
+                        <div 
+                            id="copied" 
+                            className={copy === 'unset' ? '' : (copy ? 'fade-in-out' : 'fade-in-out2')}
+                        >
+                            <p>Copied!</p>
+                        </div>
+                    </div>
+                    <div id="author">
+                        <p>- {currentQuote.author}</p>
+                    </div>
                 </div>
             </div>
             <div id="nextBtn" onClick={() => nextQuote()}>
